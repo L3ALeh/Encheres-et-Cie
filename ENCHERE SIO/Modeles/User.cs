3baf7432c38577137a0533769e515743cd7eee47
Quentin Saillard <qsaillard.ledantec@gmail.com>
2022-09-20 17:13:54 +0200

    Ajout du chiffrement du password

--- ENCHERE SIO/Modeles/User.cs
+++ ENCHERE SIO/Modeles/User.cs
@@ -1,17 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace ENCHERE_SIO.Modeles
 {
-    public class User
+    public sealed class User
     {
         #region Attributes
         public static List<User> CollClass = new List<User>();
         private int _id;
-        private string _password;
+        private SecureString _password;
         private string _mail;
         private string _nom;
         private string _prenom;
@@ -19,6 +21,11 @@ namespace ENCHERE_SIO.Modeles
 
         #region Constructor
         public User(string mail, string nom, string prenom, string password)
+            : this(mail, nom, prenom, ConvertPasswordToSecureString(password))
+        {
+
+        }
+        public User(string mail, string nom, string prenom, SecureString password)
         {
             User.CollClass.Add(this);
             _id = User.CollClass.Count() + 1;
@@ -37,6 +44,25 @@ namespace ENCHERE_SIO.Modeles
         #endregion
 
         #region Methods
+        /// <summary>
+        /// Ne laisse pas le mdp sous forme de texte brut
+        /// </summary>
+        /// <param name="password"></param>
+        /// <returns>
+        /// 1- création d'un secure string
+        /// 2- attribution caractère par caractère du password
+        /// 3- on rend immuable le mdp (ne peut être modifié)
+        /// </returns>
+        public static SecureString ConvertPasswordToSecureString(string password)
+        {
+            var secureString = new SecureString();
+            foreach (var c in password.ToCharArray())
+            {
+                secureString.AppendChar(c);
+            }
+            secureString.MakeReadOnly();
+            return secureString;
+        }
         #endregion
     }
 }
