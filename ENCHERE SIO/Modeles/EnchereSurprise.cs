91a3faf7e101d76605a45311c48f34dc59a8331e
EwenLeLoarer <ewen.leloarer1@gmail.com>
2022-09-27 14:22:01 +0200

    heritage enchere

---
+++ ENCHERE SIO/Modeles/EnchereSurprise.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ENCHERE_SIO.Modeles
+{
+    public class EnchereSurprise : Enchere
+    {
+        #region Attribut
+        public static List<EnchereSurprise> CollClasse = new List<EnchereSurprise>();
+        private double _tempsTour;
+        private List<CaseSurprise> _lesCasesSurprise;
+        #endregion
+
+        #region Constructeur
+        public EnchereSurprise(double prixDeDepart, DateTime dateFin, double prixActuel, double prixReserve, char etat, DateTime dateDebut, double tempsTour) :
+            base(prixDeDepart, dateFin, prixActuel, prixReserve, etat, dateDebut)
+        {
+            CollClasse.Add(this);
+            _tempsTour = tempsTour;
+            _lesCasesSurprise = new List<CaseSurprise>();
+        }
+
+        public double TempsTour { get => _tempsTour; set => _tempsTour = value; }
+        public List<CaseSurprise> LesCasesSurprise { get => _lesCasesSurprise; set => _lesCasesSurprise = value; }
+        #endregion
+
+        #region Getter/setter
+        #endregion
+
+        #region Methode
+        #endregion
+
+
+    }
+}
