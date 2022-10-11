923b883d9c2cc34c4d769ac8be5bd30ba92c9f84
squ1ddd <qsaillard.ledantec@gmail.com>
2022-10-11 16:25:05 +0200

    ajout de l'authentification

--- ENCHERE SIO/VuesModeles/LoginVueModele.cs
+++ ENCHERE SIO/VuesModeles/LoginVueModele.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using ENCHERE_SIO.Modeles;
+using ENCHERE_SIO.services;
+using ENCHERE_SIO.Vues;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +11,37 @@ namespace ENCHERE_SIO.VuesModeles
 {
     public class LoginVueModele:BaseVueModele
     {
+        #region Attributes
+        private readonly Api _apiServices = new Api();
+
+        private User _unUser;
+        #endregion
+
+        #region Constructor
+        public LoginVueModele()
+        {
+            this.GetUserByMailAndPass();
+        }
+        #endregion
+
+        #region Getters/Setters
+        public User UnUser 
+        { 
+            get { return _unUser; }
+            set { SetProperty(ref _unUser, value); } 
+        }
+        #endregion
+
+        #region Methods
+        public async void GetUserByMailAndPass()
+        {
+            UnUser = new User(0,"", "");
+            await _apiServices.GetOneAsync<User>("api/getUserByMailAndPass",UnUser);
+            if(UnUser != null)
+            {
+                Application.Current.MainPage = new AccueilVue();
+            }
+        }
+        #endregion
     }
 }
