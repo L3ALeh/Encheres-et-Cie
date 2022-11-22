11e9f8395f40e234e066823cff69eefe45c03e90
Quentin Saillard <qsaillard.ledantec@gmail.com>
2022-11-22 13:46:00 +0100

    ..

--- ENCHERE SIO/Vues/EnchereInverseeVue.xaml.cs
+++ ENCHERE SIO/Vues/EnchereInverseeVue.xaml.cs
@@ -1,12 +1,12 @@
 using ENCHERE_SIO.VuesModeles;
-
+using ENCHERE_SIO.Modeles;
 namespace ENCHERE_SIO.Vues;
 
 public partial class EnchereInverseeVue : ContentPage
 {
     EnchereInverseeVueModele vueModele;
 
-    public EnchereInverseeVue()
+    public EnchereInverseeVue(Enchere current)
     {
         InitializeComponent();
         BindingContext = vueModele = new EnchereInverseeVueModele();
